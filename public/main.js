206faccc8e7144c04103d461c09b3d7c3ee1417a
Corey Hall <coreyhall008@gmail.com>
2021-02-08 15:35:17 -0500

    Saving partial homework!

--- public/main.js
+++ public/main.js
@@ -1,7 +1,28 @@
 function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+  const numberOfCupsOfCoffee = 2
+
+  const fullName = 'Corey Hall'
+  console.log(
+    `My name is ${fullName} and I have had ${numberOfCupsOfCoffee} cups of coffee today!`
+  )
+  const aboutMe = {
+    favoriteMovie: ['Rush Hour 2', 'Avengers: Endgame', 'Star Wars: Episode 5'],
+    luckyNumber: 4,
   }
+  console.log(
+    `Here is a list of my my favorite movies: ${aboutMe.favoriteMovie}. Also, my lucky number is ${aboutMe.luckyNumber}.`
+  )
+  const userName = window.prompt('What is your name?')
+  console.log(`Hello, ${userName}! Nice to meet you.`)
+
+  //down below is an example for the smallest number
+  // let numbers = []
+  // let smallest
+  // for (let index = 0; index < numberOfCupsOfCoffee.length; index++){
+  //   if(smallest > variable[index])
+  //   smallest = variable[index]
+  // }
+  // console.log(smallest);
 }
 
 document.addEventListener('DOMContentLoaded', main)
